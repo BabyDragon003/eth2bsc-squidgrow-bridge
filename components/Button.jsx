385f8c8ce7cb9c58a65edbee2ea97a42c94e46b7
BabyDragon003 <golddragon01234@gmail.com>
2021-06-03 16:21:52 +0900

    "2021-06-03 16:21"

--- components/Button.jsx
+++ components/Button.jsx
@@ -3,26 +3,16 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import CliamTape from '@/assets/images/CollectWalletButton.svg';
 export const ConnectWalletBtn = ({ disabled, onClick, title }) => {
   return (
+    <div>
+      <button
+        disabled={disabled}
+        onClick={onClick}
+        className="w-full max-h-[90px] relative h-full flex flex-col justify-center items-center pink-tape"
+      >
         <span className=" !font-medium text-lg md:text-2xl z-30 w-full">
           {title}
         </span>
       </button>
-    </div>
-  );
-};
-
-import Image from 'next/image';
-
-export const NavButton = ({ isActive }) => {
-  return (
-    <div className="relative flex flex-col justify-center items-center h-full w-full pink-tape">
-      <div className="!font-medium text-lg md:text-xl">
-        <ConnectWalletButton isActive={isActive} />
-      </div>
-    </div>
-  );
-};
-export const NavigationBarBtn = ({ isActive }) => {
   return <ConnectButton />;
 };
 export const ClaimBtn = () => {
