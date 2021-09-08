764187cebabeed9d5a9afdd056e378f643a3d3ab
BabyDragon003 <golddragon01234@gmail.com>
2021-09-08 16:21:52 +0900

    "2021-09-08 16:21"

--- components/wagmi/provider.js
+++ components/wagmi/provider.js
@@ -3,6 +3,12 @@ import { useWalletClient } from "wagmi";
 import { providers } from "ethers";
 
 export function walletClientToSigner(walletClient) {
+  const { account, chain, transport } = walletClient;
+  const network = {
+    chainId: chain.id,
+    name: chain.name,
+    ensAddress: chain.contracts?.ensRegistry?.address,
+  };
   const provider = new providers.Web3Provider(transport, network);
   const signer = provider.getSigner(account.address);
   return signer;
